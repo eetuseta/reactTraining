ffd3619f6aac2c0e5890a5753f59cbcfb4b8ddb5
eetuseta <eetu.kaivola@siili.com>
2016-12-07 15:48:30 +0200

    style for about & links to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,24 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3>About app</h3>
-      <p>This app finds you temperature on given location.</p>
+      <h1 className="text-center">About app</h1>
+      <p>
+        This app finds you temperature on given location. It is built on React
+        to train myself on react.js
+      </p>
+      <p>
+        Here are some of the toos used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the
+            JavaScript framework.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open weather map</a> - I used this
+            to search weather data from city names.
+        </li>
+      </ul>
     </div>
   )
 };
