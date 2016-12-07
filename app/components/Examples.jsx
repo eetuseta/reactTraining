20eb29ab52b962cdc90bfcecea8149d8c2202a3f
eetuseta <eetu.kaivola@siili.com>
2016-12-07 13:14:30 +0200

    add paragraph to examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return(
+    <div>
     <h3>Exapmles component</h3>
+    <p>welcome to examples page!</p>
+    </div>
   )
 }
 module.exports = Examples;
