b5fb06af0ff8428053a2f11c88d9e39ea602ca8f
eetuseta <eetu.kaivola@siili.com>
2016-12-07 15:39:27 +0200

    prettified few pages

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return(
     <div>
-    <h3>Exapmles</h3>
-    <p>welcome to examples page!</p>
+    <h1 className="text-center">Exapmles</h1>
+    <p>Here are few example locations to try out:</p>
+    <ol>
+      <li>
+        <Link to='/?location=Helsinki'>Helsinki, Finland</Link>
+      </li>
+      <li>
+        <Link to='/?location=Stockholm'>Stockolm Sweden</Link>
+      </li>
+    </ol>
     </div>
   )
 }
